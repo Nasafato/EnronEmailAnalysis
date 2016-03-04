57e0f92c9b6a616e6e25a316a97e0c610734dc05
alan-kinnek <alan@kinnek.com>
2016-03-04 01:26:36 -0500

    Got classifiers and got score working.

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -4,51 +4,119 @@ import sys
 import pickle
 sys.path.append("../tools/")
 
+import pprint
+
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
-
-### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
-features_list = ['poi','salary', 'bonus', 'total_stock_value'] # You will need to use more features
+from sklearn.feature_selection import SelectKBest, f_classif
 
 ### Load the dictionary containing the dataset
 data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
 
-### Task 2: Remove outliers
-### Task 3: Create new feature(s)
+### list the features you want to look at--first item in the
+### list will be the "target" feature
+
+target_label = [
+    'poi'
+]
+
+financial_features = [
+     'salary',
+     'deferral_payments',
+     'total_payments',
+     'exercised_stock_options',
+     'bonus',
+     'restricted_stock',
+     'shared_receipt_with_poi',
+     'restricted_stock_deferred',
+     'total_stock_value',
+     'expenses',
+     'loan_advances',
+     'other',
+     'director_fees',
+     'deferred_income',
+     'long_term_incentive',
+]
+
+email_features = [
+    'from_messages',
+    'to_messages',
+    'from_poi_to_this_person',
+    'from_this_person_to_poi',
+    'shared_receipt_with_poi'
+]
+
+outliers = [
+    'TOTAL',
+    'THE TRAVEL AGENCY IN THE PARK',
+]
+
+# remove outliers from the data
+for outlier_key in outliers:
+    del data_dict[outlier_key]
+
+features_list = target_label + financial_features + email_features
+data = featureFormat(data_dict, features_list)
+target, features = targetFeatureSplit( data )
+
+# get 10 best features
+selector = SelectKBest(k=10)
+selector.fit(features, target)
+feature_names = [features_list[i] for i in selector.get_support(indices=True)]
+features_list = target_label + feature_names
+
+# poi_message_ratios - ratio of poi messages to total messages
+for name in data_dict:
+    try:
+        total_messages = data_dict[name]['from_messages'] + data_dict[name]['to_messages']
+        poi_messages = data_dict[name]['from_poi_to_this_person'] +\
+                       data_dict[name]['from_this_person_to_poi'] +\
+                       data_dict[name]['shared_receipt_with_poi']
+
+        data_dict[name]['poi_message_ratio'] = (float)(poi_messages/total_messages)
+    except (TypeError, KeyError):
+        data_dict[name]['poi_message_ratio'] = 'NaN'
+
+features_list.append('poi_message_ratio')
+
 ### Store to my_dataset for easy export below.
+my_features_list = features_list
 my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
+data = featureFormat(my_dataset, my_features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
+# scale the features
+from sklearn import preprocessing
+scaler = preprocessing.MinMaxScaler()
+features = scaler.fit_transform(features)
 
-# Provided to give you a starting point. Try a variety of classifiers.
+# create classifiers
 from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn.linear_model import LogisticRegression
+from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+nb_clf = GaussianNB()
+log_clf = LogisticRegression(C=10**18, tol=10**-21)
+kmeans_clf = KMeans(n_clusters=3, tol=0.001)
+rf_clf = RandomForestClassifier()
 
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
+log_clf.fit(features_train, labels_train)
+predictions = log_clf.predict(features_test)
+
+print(accuracy_score(labels_test, predictions))
+print(precision_score(labels_test, predictions))
+print(recall_score(labels_test, predictions))
+
+clf = log_clf
 
 dump_classifier_and_data(clf, my_dataset, features_list)
